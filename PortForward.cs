1cf873562c1e96e9455cd0fde471f79711388a45
534436673@qq.com <534436673@qq.com>
2016-04-06 18:27:01 +0800

    init

---
+++ PortForward.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class PortForward
+    {
+        private const int BUFFSIZE = 16384;
+
+        public Socket local;
+        public Socket remote;
+        public bool close = true;
+
+        private byte[] buffLocal = new byte[BUFFSIZE];
+        private byte[] buffRemote = new byte[BUFFSIZE];
+
+        public PortForward(Socket local, Socket remote)
+        {
+            this.local = local;
+            this.remote = remote;
+            close = false;
+        }
+
+        public void startForward()
+        {
+            local.BeginReceive(buffLocal, 0, BUFFSIZE, 0, new AsyncCallback(LocalRecvCallback), null);
+            remote.BeginReceive(buffRemote, 0, BUFFSIZE, 0, new AsyncCallback(RemoteRecvCallback), null);
+        }
+
+        ~PortForward()
+        {
+            //Console.WriteLine("释放成功");
+        }
+
+        public void fallback()
+        {
+            try
+            {
+                local.Close();
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.ToString());
+            }
+            try
+            {
+                remote.Close();
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.ToString());
+            }
+
+            local.Dispose();
+            remote.Dispose();
+
+            buffRemote = null;
+            buffLocal = null;
+        }
+
+        public void LocalRecvCallback(IAsyncResult ar)
+        {
+            try
+            {
+                int recv = local.EndReceive(ar);
+                if(recv > 0)
+                {
+                    this.encoder(buffLocal, recv);
+                    remote.BeginSend(buffLocal, 0, recv, 0, new AsyncCallback(LocalSendCallback), null);
+                }
+                else
+                {
+                    fallback();
+                }  
+            }
+            catch (Exception)
+            {
+                //Console.WriteLine(ex.ToString());
+                fallback();
+            }
+        }
+
+        public void LocalSendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                remote.EndSend(ar);
+                local.BeginReceive(buffLocal, 0, BUFFSIZE, 0, new AsyncCallback(LocalRecvCallback), null);
+            }
+            catch (Exception)
+            {
+                //Console.WriteLine(ex.ToString());
+                fallback();
+            }
+        }
+
+        public void RemoteRecvCallback(IAsyncResult ar)
+        {
+            try
+            {
+                int recv = remote.EndReceive(ar);
+                if (recv > 0)
+                {
+                    this.encoder(buffRemote, recv);
+                    local.BeginSend(buffRemote, 0, recv, 0, new AsyncCallback(RemoteSendCallback), null);
+                }
+                else
+                {
+                    fallback();
+                }
+            }
+            catch (Exception)
+            {
+                //Console.WriteLine(ex.ToString());
+                fallback();
+            }
+        }
+
+        public void RemoteSendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                local.EndSend(ar);
+                remote.BeginReceive(buffRemote, 0, BUFFSIZE, 0, new AsyncCallback(RemoteRecvCallback), null);
+            }
+            catch (Exception)
+            {
+                //Console.WriteLine(ex.ToString());
+                fallback();
+            }
+        }
+
+        private unsafe void encoder(byte[] str, int length)
+        {
+            if(length<=0)
+            {
+                return;
+            }
+
+            fixed(byte* source= str)
+            {
+                byte* p = source + length - 1;
+
+                while(p>=source)
+                {
+                    *p ^= 96;
+                    p--;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        const string IPADDRESS = "*";
+        const int PORT = 9001;
+        const int BACKPORT = 1090;
+
+        static void Main(string[] args)
+        {
+            Socket tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpSocket.Bind(new IPEndPoint(IPAddress.Any, BACKPORT));
+            tcpSocket.Listen(1024);
+
+            Console.WriteLine("代理服务器地址 127.0.0.1:{0:d}", BACKPORT);
+
+            tcpSocket.BeginAccept(new AsyncCallback(AcceptCallback), tcpSocket);
+
+            Console.ReadKey();
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            Socket sock = (Socket)ar.AsyncState;
+            try
+            {
+                Socket conn = sock.EndAccept(ar);
+
+                // 连接到远程服务器
+                Socket remote = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                remote.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, true);
+                remote.BeginConnect(new IPEndPoint(IPAddress.Parse(IPADDRESS), PORT), new AsyncCallback(remoteConnectedCallback), new object[] { remote, conn });               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            sock.BeginAccept(new AsyncCallback(AcceptCallback), sock);
+        }
+
+        public static void remoteConnectedCallback(IAsyncResult ar)
+        {
+            object[] obj= ((object[])ar.AsyncState);
+            Socket remote = (Socket)obj[0];
+
+            try
+            {
+                remote.EndConnect(ar);
+                remote.BeginSend(new byte[] { 1, 1, 0, 0, 0, 1, 0, 1, 1234 / 256, 1234 % 256 }, 0, 10, 0,new AsyncCallback(remoteHandShake), obj);
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static void remoteHandShake(IAsyncResult ar)
+        {
+            object[] obj = ((object[])ar.AsyncState);
+            Socket remote = (Socket)obj[0];
+            Socket conn = (Socket)obj[1];
+
+            try
+            {
+                remote.EndSend(ar);
+                // 开始转发端口
+                PortForward pf = new PortForward(conn, remote);
+                pf.startForward();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
